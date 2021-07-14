1826bb669c4087a161cfc707283306d6e54db6ac
Aaron Cohen <acohen@mountainpasstech.com>
2021-07-14 16:24:43 +0300

    improve make file

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
 # Docker
 build_image :
-	docker build -t kahunacohen/hello-kube:$(tag) .	
+	docker build -t kahunacohen/hello-kube:$(tag) .	&& docker build -t kahunacohen/helo-kube:latest .
 push_image :
-	docker push kahunacohen/hello-kube:$(tag)
+	docker push kahunacohen/hello-kube:$(tag) && docker push kahunacohen/hello-kube:latest
 run_image :
 	docker rm hello-kube && docker run -p 3000:3000 --name hello-kube hello-kube
-
 
 # minikube
 open_minikube_url :
@@ -18,7 +17,10 @@ create_configmap :
 	kubectl create configmap web-configmap --from-env-file=./.env
 create_secrets :
 	kubectl create secret generic web-secrets --from-file=./secrets
-
 set_image :
 	kubectl set image deployment/web-deployment web=kahunacohen/hello-kube:$(tag)
+
+deploy : build_image push_image set_image
+	kubectl get pods
+
 
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
+function getSecret(key) {
+  // No error handling or caching.
+  return Buffer.from(fs.readFileSync(`/etc/secrets/${key}`, {encoding: "utf-8"}), "base64")
+}
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Kubernetes Expressjs Example 0.3</h2>
+  res.send(`<h1>Kubernetes Expressjs Example 0.8</h2>
   <h2>Non-Secret Configuration Example</h2>
   <p>This uses ConfigMaps as env vars.</p>
   <ul>
@@ -13,16 +17,18 @@ app.get("/", (req, res) => {
     <li>MY_OTHER_NON_SECRET: "${process.env.MY_OTHER_NON_SECRET}"</li>
   </ul>
   <h2>Secret Config Example</h2>
-  <p>This uses actual secrets that are stored at /etc/secrets in the pod. They are created using
+  <p>This uses actual secrets that are stored at <code>/etc/secrets</code> in the pod. They are created using
   the kubectl command line program, BUT we don't create a manifest so they are not
-  in source control. The deployment mounts a volume with the base64 encoded value at /etc/secrets/{key}.txt</p>
-  <p>For demo purposes, the directory ./secrets is in VCS and there's a create_secret task in the Makefile. The secret files
-  on the host are base64 encoded. In real
-  life you'd NOT commit the secrets directory on the host or it would defeat the purpose of secrets! I've done this
-  to document how to create secrets.
+  in source control. The deployment mounts a volume with the base64 encoded value at <code>/etc/secrets/</code>. The app
+  reads the files. Each file corresponds to each secret. See below.</p>
+  <p>For demo purposes, the directory <code>./secrets</code> is in VCS and there's a <code>create_secret</code> task in the Makefile. 
+  The secret files on the host are base64 encoded, and thus they need to be decoded in the app. This is not to secure the data,
+  as they could be easily decoded. Rather they are base64 encoded to allow binary data.</p>
+  <p>In real life you'd NOT commit the secrets directory on the host or it would defeat the purpose of secrets! I've done this
+  to document how to create secrets. See the <code>Makefile</code>.</p>
   <ul>
-    <li>username: ${Buffer.from(fs.readFileSync("/etc/secrets/username", {encoding: "utf-8"}), "base64")}</li>
-    <li>password: ${Buffer.from(fs.readFileSync("/etc/secrets/password", {encoding: "utf-8"}), "base64")}</li>
+    <li>username: ${getSecret("username")}</li>
+    <li>password: ${getSecret("password")}</li>
   </ul>
   <h2>Cronjobs</h2>
   <p>You can set cronjobs which run in their own pod. Pods are not destroyed after the job completes. You can set 
