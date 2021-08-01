756b59c42edfa2e6639d9f7346a8ee6f45163caf
Aaron Cohen <acohen@mountainpasstech.com>
2021-08-01 14:30:55 +0300

    progres;

--- vault/install.sh
+++ vault/install.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
+set +e
 
 # Install and configure vault in a minikube cluster on MacOS. A minikube cluster must be running.
 
+get_num_pods() {
+  kubectl get pods -ojson | jq '.items|length'
+}
+
+# Delete pods, blocking until they are all terminated.
 kubectl delete all --all
-echo Waiting for pods to terminate
-sleep 20
+while :
+  do
+    pds=$(get_num_pods)
+    echo "Waiting for pods to terminate: $pds"
+    if [ "$pds" -eq "0" ]; then
+      break
+    fi
+    sleep .5
+  done
+
 helm uninstall consul
 helm uninstall vault
-
 
 if ! helm &> /dev/null; then
   echo Installing helm
@@ -21,24 +34,37 @@ if ! helm history consul &> /dev/null; then
   helm install consul hashicorp/consul --values helm-consul-values.yaml
 fi
 echo consul installed
-# Go into consul pod and delete db under /consul/data*
+# Go into consul pod and delete db under /consul/data* 
 
 if ! helm history vault &> /dev/null; then
   echo installing vault
   helm install vault hashicorp/vault --values helm-vault-values.yaml
-  echo waiting for vault pods to start...
-  sleep 20
 fi
 echo vault installed
+while :
+  do
+    echo Waiting for pods to start...
+    if kubectl get pods -ojson | jq -e '[.items[] | .status.phase] | length == 6 and all(.=="Running")' > /dev/null; then
+      echo all pods are running.
+      break
+    fi
+    sleep 1
+  done
+
+echo remove consul/data
+kubectl get pods
+
+# Reset consul data:
+kubectl exec consul-consul-server-0 -- rm -rf '/consul/data/*'
+sleep 5
 
 kubectl port-forward vault-0 8200:8200 &
 PORT_FWD_PID=$!
 echo port forwarding vault in background. Process id: $PORT_FWD_PID
 
-
 echo vault server status:
 kubectl exec vault-0 -- vault status
 
-# echo initializing vault server and getting keys
+echo initializing vault server and getting keys
 kubectl exec vault-0 -- vault operator init -key-shares=5 -key-threshold=5 -format=json > cluster-keys.json
-
+#kill $PORT_FWD_PID
