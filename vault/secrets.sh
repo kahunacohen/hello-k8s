deeebcb56c4620c746935bc811dcc64fff358586
Aaron Cohen <acohen@mountainpasstech.com>
2021-08-05 19:55:24 +0300

    fix

--- vault/secrets.sh
+++ vault/secrets.sh
@@ -4,20 +4,18 @@ vault secrets enable -path=secret kv-v2;
 sleep 10
 vault kv put secret/webapp/config username="static-user" password="static-password";
 sleep 10
-# Not secure, but to confirm it worked:
-vault kv get secret/webapp/config
-sleep 10
 vault auth enable kubernetes
 
 vault write auth/kubernetes/config \
         token_reviewer_jwt="$2" \
         kubernetes_host="$4" \
-        kubernetes_ca_cert="$3"
+        kubernetes_ca_cert="$3" \
+        disable_iss_validation=true
 
 sleep 10
 vault policy write myapp-kv-ro - <<EOF
-path "secret/data/webapp/*" {
-    capabilities = ["read", "list"]
+path "secret/data/webapp/config" {
+    capabilities = ["read"]
 }
 EOF
 
