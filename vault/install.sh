59ab044e388c1b9330010bcb6aa38fcc9805dd61
Aaron Cohen <acohen@mountainpasstech.com>
2021-08-10 00:04:55 +0300

    get secret working

--- vault/install.sh
+++ vault/install.sh
@@ -27,11 +27,11 @@ echo "Adding hashicorp helm repo"
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo update
 if ! helm history consul &> /dev/null; then
-  echo Installing consul 
+  echo Installing consul
   helm install consul hashicorp/consul --values helm-consul-values.yaml
 fi
 echo consul installed
-# Go into consul pod and delete db under /consul/data* 
+# Go into consul pod and delete db under /consul/data*
 
 if ! helm history vault &> /dev/null; then
   echo installing vault
@@ -57,38 +57,20 @@ sleep 10
 echo Resetting consul data.
 kubectl get pods | grep 'consul-' | awk  '{print $1}'
 pds=$(kubectl get pods | grep 'consul-' | awk  '{print $1}')
-for i in $pds; do  
-  kubectl exec "$i" -- rm -rf '/consul/data/raft'
+for i in $pds; do
+    kubectl exec "$i" -- rm -rf '/consul/data/raft'
 done;
-sleep 20
+sleep 30
 echo initializing vault server and getting keys
-kubectl exec vault-0 -- vault operator init -key-shares=5 -key-threshold=5 -format=json > cluster-keys.json
+kubectl exec vault-0 -- vault operator init -key-shares=2 -key-threshold=2 -format=json > cluster-keys.json
 
 vault_pds=$(kubectl get pods | grep -E 'vault-\d+' | awk  '{print $1}')
-for p in $vault_pds; do  
-  echo unsealing $p
-  cat cluster-keys.json | jq '.unseal_keys_hex[]' | xargs -L1 -I'{}' kubectl exec $p -- vault operator unseal '{}' > /dev/null
+for p in $vault_pds; do
+    echo unsealing $p
+    cat cluster-keys.json | jq '.unseal_keys_hex[]' | xargs -L1 -I'{}' kubectl exec $p -- vault operator unseal '{}' > /dev/null
 done;
 sleep 20
 
-
-kubectl create serviceaccount vault-auth
-sleep 10
-export VAULT_SA_NAME=$(kubectl get sa vault-auth \
-    --output jsonpath="{.secrets[*]['name']}")
-echo Get VAULT_SA_NAME
-sleep 10
-export SA_JWT_TOKEN=$(kubectl get secret $VAULT_SA_NAME \
-    --output 'go-template={{ .data.token }}' | base64 --decode)
-echo Get SA_JWT_TOKEN
-sleep 10
-export SA_CA_CRT=$(kubectl config view --raw --minify --flatten \
-    --output 'jsonpath={.clusters[].cluster.certificate-authority-data}' | base64 --decode)
-echo Get SA_CA_CRT
-sleep 10
-export K8S_HOST=$(kubectl config view --raw --minify --flatten \
-    --output 'jsonpath={.clusters[].cluster.server}')
-echo Get K8S_HOST
 root_token=$(cat cluster-keys.json | jq -r ".root_token")
 secret_script=$(cat ./secrets.sh | sed "s#\$1#$root_token#g" | sed "s#\$2#$SA_JWT_TOKEN#g" | sed "s#\$3#$SA_CA_CERT#g" | sed "s#\$4#$K8S_HOST#g")
 echo secret script:
--- vault/secrets.sh
+++ vault/secrets.sh
@@ -1,27 +1,27 @@
-vault login $1; 
-sleep 10; 
+vault login $1;
+sleep 10;
 vault secrets enable -path=secret kv-v2;
 sleep 10
 vault kv put secret/webapp/config username="static-user" password="static-password";
 sleep 10
 vault auth enable kubernetes
-
-vault write auth/kubernetes/config \
-        token_reviewer_jwt="$2" \
-        kubernetes_host="$4" \
-        kubernetes_ca_cert="$3" \
-        disable_iss_validation=true
-
 sleep 10
-vault policy write myapp-kv-ro - <<EOF
+vault write auth/kubernetes/config \
+      token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+      kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" \
+      kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
+      disable_iss_validation=true
+sleep 10
+vault policy write webapp - <<EOF
 path "secret/data/webapp/config" {
-    capabilities = ["read"]
+  capabilities = ["read"]
 }
 EOF
-
 sleep 10
+
 vault write auth/kubernetes/role/webapp \
-        bound_service_account_names=vault-auth \
-        bound_service_account_namespaces=default \
-        policies=myapp-kv-ro \
-        ttl=24h
+      bound_service_account_names=vault \
+      bound_service_account_namespaces=default \
+      policies=webapp \
+      ttl=24h
+sleep 10
