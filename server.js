b65b8996e7c64a207b4b084dbcb3716edc6778c9
Aaron Cohen <acohen@mountainpasstech.com>
2021-08-13 19:06:44 +0300

    work on client

--- server.js
+++ server.js
@@ -7,37 +7,36 @@ const axios = require('axios').default;
 
 function Vault() {
   const baseUrl = `${process.env.VAULT_ADDR}/v1`;
+
   const getHealth = async () => {
-    try {
-      return await axios.get(`${baseUrl}/sys/health?standbyok=true`);
-    } catch(e) {
-      return null;
-    }
+    const resp = await axios.get(`${baseUrl}/sys/health?standbyok=true`);
+    return resp.data;
+  }
+  
+  const getAPIToken = () => {
+    return fs.readFileSync(process.env.JWT_PATH, {encoding: "utf-8"});
+  }
+
+  const getVaultToken = async () => {
+    const resp = await axios.post(`${baseUrl}/auth/kubernetes/login`, {jwt: getAPIToken(), role: "webapp"});
+    return resp.data;
+    //curl --request POST --data '{"jwt": "eyJh", "role": "webapp"}' http://vault:8200/v1/auth/kubernetes/login
   }
   return {
-    getHealth
+    getAPIToken,
+    getHealth,
+    getVaultToken
   }
+
 }
 
-const vault = Vault(); 
+const vault = Vault();
 app.get("/", async (req, res) => {
-  // const jwt = fs.readFileSync(process.env.JWT_PATH, {encoding: "utf-8"});
-  // console.log(`token: ${jwt}`);
-  // const health = await getVaultHealth();
-  // console.log(`health: ${health}`);
-  // if (health !== 200) {
-  //   res.send(`<p>Health is: ${health}`);
-  //   return;
-  // }
-
-  // axios.post(`${process.env.VAULT_ADDR}/v1/auth/kubernetes/login`, {
-  //   jwt,
-  //   role: "webapp"
-  // }).then(x => console.log(x))
-  //   .catch(function (error) {
-  //     console.log("HERE IS THE ERROR:")
-  //     console.log(error.toJSON());
-  // });
+  const health = await vault.getHealth();
+  console.log("health:");
+  console.log(health);
+  console.log("vault token:");
+  console.log(await vault.getVaultToken());
   res.send(`<h1>Kubernetes Expressjs Test settings</h2>
   <h2>Non-Secret Configuration Example</h2>
   <p>This uses ConfigMaps as env vars.</p>
