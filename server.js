2194f6ff0ec2655bba13639ac53902954a9da95d
Aaron Cohen <acohen@mountainpasstech.com>
2021-09-19 17:51:12 +0300

    clean up

--- server.js
+++ server.js
@@ -2,87 +2,92 @@ const process = require("process");
 const express = require("express");
 const app = express();
 const fs = require("fs");
-const axios = require('axios').default;
-const { Pool, Client } = require('pg')
-
+const axios = require("axios").default;
+const { Pool} = require("pg");
 
 function Vault() {
-  const axiosInst = axios.create({baseURL: `${process.env.VAULT_ADDR}/v1`});
+  const axiosInst = axios.create({ baseURL: `${process.env.VAULT_ADDR}/v1` });
   const getHealth = async () => {
     const resp = await axisoInst.get(`/sys/health?standbyok=true`);
     return resp.data;
-  }
-  
+  };
+
   const getAPIToken = () => {
-    return fs.readFileSync(process.env.JWT_PATH, {encoding: "utf-8"});
-  }
+    return fs.readFileSync(process.env.JWT_PATH, { encoding: "utf-8" });
+  };
   const getVaultAuth = async (role) => {
-    const resp = await axiosInst.post("/auth/kubernetes/login", {jwt: getAPIToken(), role});
+    const resp = await axiosInst.post("/auth/kubernetes/login", {
+      jwt: getAPIToken(),
+      role,
+    });
     return resp.data;
-  }
+  };
   const getSecrets = async (vaultToken) => {
-    const resp = await axiosInst("/secret/data/webapp/config", {headers: {"X-Vault-Token": vaultToken}});
+    const resp = await axiosInst("/secret/data/webapp/config", {
+      headers: { "X-Vault-Token": vaultToken },
+    });
     return resp.data.data.data;
-  }
+  };
   return {
     getAPIToken,
     getHealth,
     getSecrets,
-    getVaultAuth
-  }
+    getVaultAuth,
+  };
 }
 
 function getPool() {
   return new Pool({
-    user: 'web',
-    host: 'mypostgres',
-    database: 'web',
-    password: 'akhd5',
+    user: "web",
+    host: "mypostgres",
+    database: "web",
+    password: "akhd5",
     port: 5432,
   });
 }
 function createFilmsTable(pool) {
-  pool.query(`
+  pool.query(
+    `
   CREATE TABLE IF NOT EXISTS films (
     title       varchar(40) NOT NULL,
     kind        varchar(10)
-);`, (_err, _queryRes) => {
-
-  });
+);`,
+    (_err, _queryRes) => {}
+  );
 }
 
 function getFilms(pool) {
   return new Promise((resolve) => {
-    pool.query(`SELECT title, kind FROM "films";`, (_, queryRes) => {
+    pool.query(`SELECT title, kind FROM "films";`, (err, queryRes) => {
       resolve(queryRes.rows);
     });
-  })
+  });
 }
 async function populateFilmsTable(pool) {
   const films = await getFilms(pool);
   if (films.length === 0) {
-    pool.query(`INSERT INTO "films"(title, kind)
-    VALUES('Superman', 'drama');`, (_, queryRes) => {
-      return queryRes.rows;
-    });
+    pool.query(
+      `INSERT INTO "films"(title, kind)
+    VALUES('Superman', 'drama');`,
+      (_, queryRes) => {
+        return queryRes.rows;
+      }
+    );
   }
 }
 
 const vault = Vault();
 let pool = getPool();
-
 createFilmsTable(pool);
-app.get("/", async (req, res) => {
-  
-await populateFilmsTable(pool);
-const films = await getFilms(pool);
-pool.end();
-let trs = "<tr>";
-for (const film of films) {
-  trs += `<td>${film.title}</td><td>${film.kind}</td>`;
-}
-trs += "</tr>";
-const html = `
+app.get("/", async (req, res, next) => {
+  await populateFilmsTable(pool);
+  const films = await getFilms(pool);
+  let trs = "<tr>";
+  for (const film of films) {
+    trs += `<td>${film.title}</td><td>${film.kind}</td>`;
+  }
+  trs += "</tr>";
+  const html = `
   <table border=1>
     <thead>
       <th>Title</th>
@@ -93,10 +98,9 @@ const html = `
     </tbody>
   </table>
   `;
-console.log(html);
-res.send(html);
-
-
+  // console.log(html);
+  res.send(html);
+  next();
 
   // const vaultAuth = await vault.getVaultAuth("webapp");
   // const secrets = await vault.getSecrets(vaultAuth.auth.client_token);
@@ -115,8 +119,6 @@ res.send(html);
   // </ul>
   // `);
 });
-
-
 app.listen(3000, () => {
   console.log("Listening on http://localhost:3000");
 });
