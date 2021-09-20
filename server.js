04556c52dfd147e2b3f2f5dbad9a12602e5ab260
Aaron Cohen <acohen@mountainpasstech.com>
2021-09-20 15:26:05 +0300

    get secrets in json

--- server.js
+++ server.js
@@ -9,26 +9,26 @@ const {
   getFilms,
   getPool,
 } = require("./film");
-const path = require('path');
-const mustache = require("mustache")
+const path = require("path");
+const mustache = require("mustache");
 
-app.engine('html', function (filePath, options, callback) {
+app.set("json spaces", 2);
+app.engine("html", function (filePath, options, callback) {
   fs.readFile(filePath, function (err, content) {
-    if (err)
-      return callback(err)
+    if (err) return callback(err);
     var rendered = mustache.render(content.toString(), options);
-    return callback(null, rendered)
+    return callback(null, rendered);
   });
 });
 
 // Setting mustachejs as view engine
-app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "html");
+app.set("views", path.join(__dirname, "views"));
 
 function Vault() {
   const axiosInst = axios.create({ baseURL: `${process.env.VAULT_ADDR}/v1` });
   const getHealth = async () => {
-    const resp = await axisoInst.get(`/sys/health?standbyok=true`);
+    const resp = await axiosInst.get(`/sys/health?standbyok=true`);
     return resp.data;
   };
 
@@ -56,39 +56,51 @@ function Vault() {
   };
 }
 
-const vault = Vault();
+
 let pool = getPool();
 createFilmsTable(pool);
 
-app.get('/sample', (req, res) => {
-  res.render('sample.html', { data: 'Sample Data' });
+app.get("/config", async (req, res) => {
+  const vault = Vault();
+  const vaultAuth = await vault.getVaultAuth("webapp");
+  const secrets = await vault.getSecrets(vaultAuth.auth.client_token);
+  
+  
+  res.json({
+    MY_NON_SECRET: process.env.MY_NON_SECRET,
+    MY_OTHER_NON_SECRET: process.env.MY_OTHER_NON_SECRET,
+    username: secrets.username,
+    password: secrets.password
+  });
+  // res.render('sample.html', { data: 'Sample Data' });
 });
-app.get("/", async (req, res, next) => {
+app.get("/films", async (req, res, next) => {
   let films = await getFilms(pool);
   if (films.length === 0) {
     await populateFilmsTable(pool);
     films = await getFilms();
   }
+  res.json(films);
 
-  let trs = "";
-  for (const film of films) {
-    trs += `<tr><td>${film.title}</td><td>${film.kind}</td></tr>`;
-  }
+  // let trs = "";
+  // for (const film of films) {
+  //   trs += `<tr><td>${film.title}</td><td>${film.kind}</td></tr>`;
+  // }
 
-  const html = `
-  <h2>Films</h2>
-  <table border=1>
-    <thead>
-      <th>Title</th>
-      <th>Kind</th>
-    </thead>
-    <tbody>
-      ${trs}
-    </tbody>
-  </table>
-  `;
-  res.send(html);
-  next();
+  // const html = `
+  // <h2>Films</h2>
+  // <table border=1>
+  //   <thead>
+  //     <th>Title</th>
+  //     <th>Kind</th>
+  //   </thead>
+  //   <tbody>
+  //     ${trs}
+  //   </tbody>
+  // </table>
+  // `;
+  // res.send(html);
+  // next();
 
   // const vaultAuth = await vault.getVaultAuth("webapp");
   // const secrets = await vault.getSecrets(vaultAuth.auth.client_token);
