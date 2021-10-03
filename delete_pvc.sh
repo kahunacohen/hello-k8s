8791158a248ed769525e14419583ffd13b4b5ab2
Aaron Cohen <acohen@mountainpasstech.com>
2021-10-03 17:54:21 +0300

    Added delete

--- delete_pvc.sh
+++ delete_pvc.sh
@@ -1,3 +1,5 @@
+kubectl delete ns kubegres-system
 kubectl delete pvc postgres-db-mypostgres-3-0 
 kubectl delete pvc postgres-db-mypostgres-2-0
 kubectl delete pvc postgres-db-mypostgres-1-0  
+kubectl apply -f https://raw.githubusercontent.com/reactive-tech/kubegres/v1.11/kubegres.yaml
